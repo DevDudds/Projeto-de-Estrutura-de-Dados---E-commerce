964998879aaca37ae1937eb850aa0743b669023f
dudds <duddscosta@gmail.com>
2026-07-12 18:04:09 -0300

    feat(G-1): Adiciona main.c com menu interativo

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    int opcao;
+
+    do {
+        printf("\nDigite uma opção");
+        printf("\n\t1 - Inserir\n\t2 - Remover\n\t3 - Listar\n\t4 - Sair\n");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+            case 1:
+
+            break;
+            case 2:
+
+            break;
+            case 3:
+
+            break;
+            case 4:
+                return 0;
+            break;
+            default:
+                continue;
+            break;
+        }
+    } while (opcao != 4);
+} 
